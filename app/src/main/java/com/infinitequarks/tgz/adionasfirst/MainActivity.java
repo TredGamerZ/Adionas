7b7fb8f01bafa158bed29962a122596f339cbed7
Tanmay Singh <tanmaysingh1226@gmail.com>
2017-12-21 15:03:19 +0530

    Init

---
+++ app/src/main/java/com/infinitequarks/tgz/adionasfirst/MainActivity.java
@@ -0,0 +1,49 @@
+package com.infinitequarks.tgz.adionasfirst;
+
+import android.app.DownloadManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    // Instantiate the RequestQueue.
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://10.0.0.100:3000";
+        final TextView mTextView = (TextView) findViewById(R.id.headline);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: "+ response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+
+        queue.add(stringRequest);
+
+    }
+}
