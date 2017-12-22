da00e5b20fa2bc3f5742d581087bacd262cfad97
priyanshu.s <priyanshu.s@media.net>
2017-12-22 17:46:16 +0530

    jsonObject request

--- app/src/main/java/com/infinitequarks/tgz/adionasfirst/MainActivity.java
+++ app/src/main/java/com/infinitequarks/tgz/adionasfirst/MainActivity.java
@@ -1,23 +1,38 @@
 package com.infinitequarks.tgz.adionasfirst;
 
+import android.*;
+import android.Manifest;
 import android.app.DownloadManager;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 
 public class MainActivity extends AppCompatActivity {
     // Instantiate the RequestQueue.
-
+    private FusedLocationProviderClient mFusedLocationClient;
 
 
     @Override
@@ -25,10 +40,76 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://10.0.0.100:3000";
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        final String url = "http://10.0.0.100:3000";
         final TextView mTextView = (TextView) findViewById(R.id.headline);
+        final JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("bye", 1);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PackageManager.GET_PERMISSIONS);
+//            Log.e("adsda", "Fucked bro!!!!");
+            return;
+        }
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            try {
+                                jsonObject.put("long", location.getLongitude());
+                                jsonObject.put("lat", location.getLatitude());
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, jsonObject, new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    try {
+                                        mTextView.setText("Response is: "+ response.get("hello"));
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }, new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    mTextView.setText("That didn't work!");
+                                }
+                            });
+                            queue.add(jsonObjectRequest);
+                        }
+                    }
+                });
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, jsonObject, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    mTextView.setText("Response is: "+ response.get("hello"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
@@ -43,7 +124,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        queue.add(stringRequest);
 
     }
 }
